de999c6947fd1ec8faba780a589b471c4bdd9c2b
z4qu <59421804+z4qu@users.noreply.github.com>
2023-01-19 23:23:43 +0100

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace HelloWorll
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            double a = 20;
+            double b = 10;
+
+            Math math = new Math();
+            double addResult = math.Add(a, b);
+            
+            double substractResult = math.Substract(a, b);
+            
+            Console.WriteLine(addResult.ToString());
+            
+            Console.WriteLine(substractResult.ToString());
+        }
+    }
+    
+}
---
+++ ConsoleApp2/math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace HelloWorll
+{
+    public class Math
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
